e9de65ff3175dc1aa7b47f7b3e41ac7140791bfe
aydemirberk <beerkaydemir@gmail.com>
2022-06-28 01:34:02 +0300

    Added Console Output For Every Revolution
    If a planet completed its revolution around its orbit. It will be written on Console.
    
    Added Skybox

--- Bootcamp3Project/Assets/Scripts/Rotate.cs
+++ Bootcamp3Project/Assets/Scripts/Rotate.cs
@@ -6,15 +6,23 @@ public class Rotate : MonoBehaviour
 {
     [SerializeField]
     float rotationSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float totalRotationAngle;
+
 
     // Update is called once per frame
     void Update()
     {
+        // For rotating planets.
         transform.Rotate(new Vector3(0, rotationSpeed, 0) * Time.deltaTime);
+
+
+        // Calculates total rotation angle and writes to console if planet has completed the revolution.
+        totalRotationAngle += Time.deltaTime * rotationSpeed;
+
+        if (totalRotationAngle >= 360)
+        {
+            totalRotationAngle = 0;
+            Debug.Log($"{gameObject.name} has completed one revolution." );
+        }
     }
 }
